68eb1072e055993c5ac55b82752d7044898884e3
VERAJUAN <verajuan78@gmail.com>
2019-09-22 18:26:20 -0300

    Obtener coordenadas del comercio

--- Trabajos/Trabajos Prácticos/Prácticos Evaluables/TP06/Implementación/js/app.js
+++ Trabajos/Trabajos Prácticos/Prácticos Evaluables/TP06/Implementación/js/app.js
@@ -1,8 +1,38 @@
 var app = angular.module("myApp", []);
 
-app.controller("myCtrl", function($scope) {
+app.controller("myCtrl", function($scope,$http) {
 
   $scope.momentoEntrega = "" // 'A' lo antes posible, 'P' hora programada
   $scope.metodoPago = "" // 'T' tarjeta de credito, 'E' efectivo
+
+  $scope.obtenerCoordenadasComercio = function () {
+      $http.get('https://maps.googleapis.com/maps/api/geocode/json?address='
+      + $scope.alturaComercio + '+'
+      + $scope.calleComercio + ',+'
+      + $scope.ciudadComercio +
+      ',+cordoba'+
+      '&key=AIzaSyDWm21zrmC4NZfNez65rFSmHrugggF5rAY')
+
+      .then(function (respuesta) {
+          console.log(respuesta.data.results[0].geometry.location)
+      })
+  }
+
+  $scope.obtenerDireccionComercio = function () {
+      $http.get('https://maps.googleapis.com/maps/api/geocode/json?address='
+      + $scope.alturaComercio + '+'
+      + $scope.calleComercio + ',+'
+      + $scope.ciudadComercio +
+      ',+cordoba'+
+      '&key=AIzaSyDWm21zrmC4NZfNez65rFSmHrugggF5rAY')
+
+      .then(function (respuesta) {
+          console.log(respuesta.data.results[0].geometry.location)
+      })
+  }
+
+
+
+
 
 });
