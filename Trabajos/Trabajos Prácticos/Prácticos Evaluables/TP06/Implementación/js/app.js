aea3fbf7a266390ea8ab404d46c167a27479aeba
VERAJUAN <verajuan78@gmail.com>
2019-09-22 13:40:12 -0300

    Border al seleccionar boton de metodo de pago

--- Trabajos/Trabajos Prácticos/Prácticos Evaluables/TP06/Implementación/js/app.js
+++ Trabajos/Trabajos Prácticos/Prácticos Evaluables/TP06/Implementación/js/app.js
@@ -2,7 +2,7 @@ var app = angular.module("myApp", []);
 
 app.controller("myCtrl", function($scope) {
 
-  $scope.momentoEntrega = "A" // 'A' lo antes posible, 'P' hora programada
+  $scope.momentoEntrega = "" // 'A' lo antes posible, 'P' hora programada
   $scope.metodoPago = "" // 'T' tarjeta de credito, 'E' efectivo
 
 });
