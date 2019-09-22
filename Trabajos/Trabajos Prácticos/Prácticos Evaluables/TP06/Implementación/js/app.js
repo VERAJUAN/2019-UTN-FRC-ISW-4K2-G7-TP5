e22881e9d9ef92ec1f6a119036d0f63789d9414f
Rodrigo FV <rodrigofonseca356@gmail.com>
2019-09-22 18:07:16 -0300

    Agregada funcionalidad para cargar imagen

--- Trabajos/Trabajos Prácticos/Prácticos Evaluables/TP06/Implementación/js/app.js
+++ Trabajos/Trabajos Prácticos/Prácticos Evaluables/TP06/Implementación/js/app.js
@@ -1,8 +1,29 @@
 var app = angular.module("myApp", []);
 
-app.controller("myCtrl", function($scope) {
+app.controller("myCtrl", function ($scope) {
 
-  $scope.momentoEntrega = "" // 'A' lo antes posible, 'P' hora programada
-  $scope.metodoPago = "" // 'T' tarjeta de credito, 'E' efectivo
+    $scope.momentoEntrega = "" // 'A' lo antes posible, 'P' hora programada
+    $scope.metodoPago = "" // 'T' tarjeta de credito, 'E' efectivo
+    $scope.cargarImagen = function () {
+        if (window.FileReader) {
+            document.getElementById('upload-photo').addEventListener('change', handleFileSelect, false);
+            function handleFileSelect(evt) {
+                var files = evt.target.files;
+                var f = files[0];
+                var reader = new FileReader();
 
+                reader.onload = (function (theFile) {
+                    return function (e) {
+                        document.getElementById('list').innerHTML = ['<img src="', e.target.result, '" title="', theFile.name, '" width="300" />'].join('');
+                    };
+                })(f);
+
+                reader.readAsDataURL(f);
+            }
+
+        } else {
+            alert('This browser does not support FileReader');
+        }
+    }
+    $scope.cargarImagen();
 });
